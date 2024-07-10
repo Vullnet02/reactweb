08c437bdf5be32d338814ab52ee7597c522453b9
Vullnet Jakupi <130916457+Vullnet02@users.noreply.github.com>
2024-07-10 11:06:18 +0200

    some change

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Style from "../Style/navbar.module.css";
+const Navbar = () => {
+  return (
+    <div className={Style.MainWrapper}>
+      <p className={Style.paragrafOne}>Nabar</p>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Test from "../components/Test";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Test />
+    </>
+  );
+};
+
+export default Home;
